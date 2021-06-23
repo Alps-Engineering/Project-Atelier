f33f5f41b1fe664651d8e6586d2b87f87bbda1a1
timapar <timothy_parrish@hotmail.com>
2021-06-23 19:38:06 -0400

    create model for reviews documents

---
+++ ReviewsAPI/Mongo/models/reviewsModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const db = require('../database');
+
+const reviewsSchema = new mongoose.Schema({
+  review_id: { type: Number, unique: true, required: true },
+  product_id: { type: Number, index: true },
+  rating: { type: Number, index: true },
+  summary: String,
+  body: String,
+  recommend: { type: Boolean, index: true },
+  response: String,
+  date: { type: String, index: true },
+  reviewer_name: String,
+  helpfulness: { type: Number, index: true },
+  email: String,
+  reported: { type: Boolean, index: true },
+  characteristics: [Mixed],
+  photos: [Mixed],
+});
+
+module.exports.Review = db.model('Review', reviewsSchema);
