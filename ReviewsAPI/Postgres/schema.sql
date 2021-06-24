cc16b3f37c75d958e60cf6275e2634b429363383
timapar <timothy_parrish@hotmail.com>
2021-06-24 11:56:23 -0400

    remove foreign keys and elongate summary to accommodate dirty data

--- ReviewsAPI/Postgres/schema.sql
+++ ReviewsAPI/Postgres/schema.sql
@@ -8,9 +8,9 @@ CREATE TABLE reviews (
   recommend boolean,
   reported boolean,
   reviewer_name CHAR ( 64 ),
-  email CHAR ( 64 ),
+  reviewer_email CHAR ( 64 ),
   date CHAR ( 32 ),
-	summary VARCHAR ( 64 ),
+	summary VARCHAR ( 128 ),
 	body VARCHAR ( 1024 ),
   response VARCHAR ( 1024 ),
   characteristics hstore
@@ -22,13 +22,9 @@ CREATE TABLE photos (
   url VARCHAR ( 512 )
 );
 
-ALTER TABLE photos ADD CONSTRAINT photos_review_id_fkey FOREIGN KEY (review_id) REFERENCES reviews(review_id);
-
 CREATE TABLE metadata (
   product_id SERIAL PRIMARY KEY,
   ratings hstore,
   recommended hstore,
   characteristics hstore
 );
-
-ALTER TABLE reviews ADD CONSTRAINT reviews_product_id_fkey FOREIGN KEY (product_id) REFERENCES metadata(product_id);
