aa5a48c40033f98d464db1534538900610384576
timapar <timothy_parrish@hotmail.com>
2021-06-24 19:41:37 -0400

    import the config file correctly

--- ReviewsAPI/Postgres/database.js
+++ ReviewsAPI/Postgres/database.js
@@ -1,14 +1,14 @@
 const { Pool, Client } = require('pg');
-const config = require('../../config');
+const { postgresConfig } = require('../../config');
 
-module.exports.pool = new Pool(config);
+module.exports.pool = new Pool(postgresConfig);
 
 // pool.query('SELECT NOW()', (err, res) => {
 //   console.log(err, res);
 //   pool.end();
 // });
 
-module.exports.client = new Client(config);
+module.exports.client = new Client(postgresConfig);
 
 // client.connect();
 // client.query('SELECT NOW()', (err, res) => {
