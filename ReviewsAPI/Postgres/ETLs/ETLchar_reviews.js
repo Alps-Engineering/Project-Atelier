b9345c6f1a4abdff311b12c489e0975a40242428
timapar <timothy_parrish@hotmail.com>
2021-06-25 11:57:02 -0400

    tentatively believe this is implemented

--- ReviewsAPI/Postgres/ETLs/ETLchar_reviews.js
+++ ReviewsAPI/Postgres/ETLs/ETLchar_reviews.js
@@ -6,30 +6,34 @@ pool.connect((err, client, release) => {
   if (err) {
     return console.error('Error acquiring client', err.stack);
   }
-  fs.createReadStream(
-    '/home/tim/HackReactor/Project-Atelier/ReviewsAPI/data/characteristic_reviewsTest.csv',
-  )
-    .pipe(
-      csv({
-        mapHeaders: ({ header }) => {
-          if (header === 'id') return null;
-          if (header === 'characteristic_id') return 'id';
-          return header;
-        },
-      }),
-    )
-    .on('data', (data) => {
-      client.query(
-        `UPDATE reviews SET characteristics = '${JSON.stringify({
-          id: data.id,
-          value: data.value,
-        })}' WHERE review_id = ${data.review_id};`,
-        (err, result) => {
-          if (err) return console.error('Error executing query', err.stack);
-        },
-      );
-    })
-    .on('end', () => {
-      console.log('done');
-    });
+  client.query("UPDATE reviews SET characteristics = hstore(ARRAY[['id-name', 'value']]);", (err, result) => {
+    if (err) {
+      return console.error('Error executing hstore dummy data query', err.stack);
+    }
+    fs.createReadStream('/home/tim/HackReactor/Project-Atelier/ReviewsAPI/data/characteristic_reviewsTest.csv')
+      .pipe(
+        csv({
+          mapHeaders: ({ header }) => {
+            if (header === 'id') return null;
+            if (header === 'characteristic_id') return 'id';
+            return header;
+          },
+        }),
+      )
+      .on('data', (data) => {
+        client.query(
+          `UPDATE reviews SET characteristics = characteristics || hstore(ARRAY[['id', '${data.id}'], ['value', '${
+            data.value
+          }']]) WHERE review_id = ${Number(data.review_id)};`,
+          (err, result) => {
+            if (err) {
+              return console.error(`Error executing query on review: ${data.review_id}`, err.stack);
+            }
+          },
+        );
+      })
+      .on('end', () => {
+        console.log('done');
+      });
+  });
 });
