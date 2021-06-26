22a1065643b9ac2b952025b768540e0750bbc521
Austin <austinm2040@gmail.com>
2021-06-26 11:10:46 -0400

    All APIs working with correct format; ticket complete

--- ProductsAPI/MySQL/routes.js
+++ ProductsAPI/MySQL/routes.js
@@ -13,6 +13,7 @@ router.route('/').get((req, res) => {
   let queryStr = `SELECT product_id AS id, product_name AS name, slogan, \
                   product_description AS description, category, default_price FROM products \
                   LIMIT ${count} OFFSET ${(page * count) - count}`; //OFFSET value allows us to specify which row to start from retrieving data
+
   db.query(queryStr, (err, result) => {
     if (err) {
       throw err;
@@ -28,6 +29,7 @@ router.route('/:product_id').get((req, res) => {
                   products.product_description AS description, products.category, products.default_price, \
                   JSON_OBJECTAGG(features.feature, features.feature_value) AS features FROM products, features \
                   WHERE products.product_id = ${id} AND features.product_id = ${id} GROUP BY products.product_id`;
+
   db.query(queryStr, id, (err, result) => {
     if (err) {
       throw err;
@@ -43,16 +45,7 @@ router.route('/:product_id').get((req, res) => {
       res.send(result[0]);
     }
   });
-  // try {
-  //   const rows = await db.query(queryStr)
-  //   console.log('result: ', rows)
-  // } catch (err) {
-  //   console.log('this is error:', err);
-  // };
 });
-
-//photos: array of objects photos: [ { thumbnail_url: '', url: '' } ],
-//skus: object of objects skus: { sku_id: { quantity: '', size: '' } }
 
 router.route('/:product_id/styles').get((req, res) => {
   let id = [req.params.product_id];
@@ -65,7 +58,6 @@ router.route('/:product_id/styles').get((req, res) => {
     return new Promise((resolve, reject) => {
       db.query(stylesQuery, id, (err, result) => {
         if (err) {
-          // reject(err.code);
           reject(`Data cannot properly load:  ${err.code}`);
         } else {
           resolve(result);
@@ -145,91 +137,18 @@ router.route('/:product_id/styles').get((req, res) => {
   .catch(err => {
     res.send(err);
   });
-
-
-
-  // let id = [req.params.product_id];
-  // let styleId = [req.body.style_id];
-  // let queryStr = `SELECT style_id, style_name AS name, original_price, sale_price, default_style FROM styles WHERE product_id = ${id}`;
-  // let queryStr = `SELECT s.style_id, s.style_name, s.original_price, s.sale_price, s.default_style, ph.thumbnail_url, ph.photo_url, \
-  //                 sk.sku_id, sk.quantity, sk.size FROM styles s JOIN products p ON p.product_id=s.product_id JOIN photos ph ON ph.style_id=s.style_id \
-  //                 JOIN skus sk ON sk.style_id=ph.style_id WHERE p.product_id = ${id}`;
-  // let results;
-
-  // const getPhotos = async () => {
-  //   queryStr = `SELECT thumbnail_url, photo_url AS url FROM photos WHERE style_id = ${id}`;
-  //   for (let i = 0; i < results.length; i++) {
-  //     id = [results[i].style_id];
-  //     await db
-  //     .connect()
-  //     try {
-  //     .query(queryStr, id)
-  //     .then(res => {
-  //       client.release();
-  //       results[i].photos = res;
-  //     })
-  //     .catch(e => {
-  //       client.release();
-  //       console.error(e.stack);
-  //     }));
-
-  //   }
-
-  // }
-  // const getSkus = () => {
-  //   let skusQuery = `SELECT quantity, size FROM skus WHERE style_id = ?`;
-  //   for (let i = 0; i < results.length; i++) {
-  //     styleId = [results[i].style_id];
-  //     db.query(skusQuery, styleId, (err, result) => {
-  //       if (err) {
-  //         throw err;
-  //       } else {
-  //         console.log('skusResult', result)
-  //         results[i].skus += result;
-  //       }
-  //     });
-  //   }
-  // }
-  // db.query(queryStr, id, (err, result) => {
-  //   if (err) {
-  //     throw err;
-  //   } else {
-  //     result.map((obj, i) => {
-  //       if (result[i].default_style === 1) {
-  //         delete(result[i].default_style);
-  //         result[i]['default?'] = true
-  //       } else {
-  //         delete(result[i].default_style);
-  //         result[i]['default?'] = false;
-  //       }
-  //       return (
-  //         result[i].photos = [
-  //           {
-  //             thumbnail_url: 'test',
-  //             url: 'test'
-  //           }
-  //         ],
-  //         result[i].skus = {
-  //           'sku_id': {
-  //             quantity: 'test',
-  //             size: 'test'
-  //           }
-  //         }
-  //       )
-  //     });
-  //     console.log('resultsArray', result)
-  //     res.send({ 'product_id': id[0], 'results': result });
-  //   }
-  // });
 });
 
 router.route('/:product_id/related').get((req, res) => {
-  let queryStr = `SELECT * FROM related ORDER BY related_id DESC LIMIT 1`;
+  let id = [req.params.product_id];
+  let queryStr = `SELECT JSON_ARRAYAGG(related_product_id) AS related fROM related WHERE current_product_id = ${id}`;
+
   db.query(queryStr, (err, result) => {
     if (err) {
       throw err;
     } else {
-      res.send(result);
+      let relatedArray = JSON.parse(result[0].related);
+      res.send(relatedArray);
     }
   });
 });
