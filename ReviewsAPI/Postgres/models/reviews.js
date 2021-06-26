44f5687aa381ad377e6eedffd5f53fe03b69cd79
timapar <timothy_parrish@hotmail.com>
2021-06-26 17:03:17 -0400

    progress made
    still doesn't handle multiple photos
    response is a string that says null instead of being null

---
+++ ReviewsAPI/Postgres/models/reviews.js
@@ -0,0 +1,53 @@
+const { pool } = require('../database');
+
+module.exports.getReviewsByProduct = (product, sort, page, count, cb) => {
+  pool.query(
+    `
+    SELECT r.review_id, r.rating, r.helpfulness, r.recommend, r.reviewer_name, r.date, r.summary, r.body, r.response, p.id, p.url
+    FROM reviews r
+    LEFT JOIN photos p
+    ON r.review_id = p.review_id
+    WHERE r.product_id = ${product}
+    AND r.reported = false;
+    `,
+    (err, result) => {
+      if (err) {
+        console.error('There was a problem getting the reviews from the db: ', err.stack);
+        cb(err);
+      }
+      if (result) {
+        const results = [];
+        result.rows.forEach((review) => {
+          const formatedReview = { ...review };
+          formatedReview.photos = [];
+          if (formatedReview.id) {
+            formatedReview.photos.push({ id: review.id, url: review.url });
+          }
+          delete formatedReview.id;
+          delete formatedReview.url;
+          results.push(formatedReview);
+        });
+        cb(null, {
+          product,
+          page,
+          count,
+          results,
+        });
+      }
+    },
+  );
+};
+
+module.exports.postReviewByProduct = (product, data, cb) => {
+  pool.query(
+    `
+
+    `,
+    (err, result) => {
+      if (err) {
+        console.error('There was a problem writing the data to the db: ', err.stack);
+        cb(err);
+      }
+    },
+  );
+};
