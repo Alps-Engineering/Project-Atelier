8557642bdbf9efd80075623b664a092ec5a9fb68
timapar <timothy_parrish@hotmail.com>
2021-06-28 11:57:28 -0400

    implement query to update db

--- ReviewsAPI/Postgres/models/helpful.js
+++ ReviewsAPI/Postgres/models/helpful.js
@@ -1,3 +1,29 @@
 const { pool } = require('../database');
 
-module.exports;
+module.exports.markReviewHelpful = (review_id, cb) => {
+  const selectQryStr = `
+  SELECT helpfulness
+  FROM reviews
+  WHERE review_id = ${review_id};`;
+
+  pool.query(selectQryStr, (err, result) => {
+    if (err) {
+      console.error('There was a problem getting the data from the db: ', err.stack);
+      cb(err);
+    }
+    if (result) {
+      const updateQryStr = `
+      UPDATE reviews
+      SET helpfulness = ${result.rows[0].helpfulness + 1}
+      WHERE review_id = ${review_id};`;
+
+      pool.query(updateQryStr, (err, result) => {
+        if (err) {
+          console.error('There was a problem inserting the data: ', err.stack);
+          cb(err);
+        }
+        cb(null, result);
+      });
+    }
+  });
+};
--- ReviewsAPI/Postgres/models/report.js
+++ ReviewsAPI/Postgres/models/report.js
@@ -1,3 +1,18 @@
 const { pool } = require('../database');
 
-module.exports;
+module.exports.markReviewReported = (review_id, cb) => {
+  const updateQryStr = `
+  UPDATE reviews
+  SET reported = true
+  WHERE review_id = ${review_id};`;
+
+  pool.query(updateQryStr, (err, result) => {
+    if (err) {
+      console.error('There was a problem updating the data: ', err.stack);
+      cb(err);
+    }
+    if (result) {
+      cb(null, result);
+    }
+  });
+};
