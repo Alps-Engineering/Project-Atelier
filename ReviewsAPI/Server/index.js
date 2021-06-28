150a7877b11d9664c712dc47a74c6c142ce1bf8b
timapar <timothy_parrish@hotmail.com>
2021-06-28 11:56:51 -0400

    remove individual routes

--- ReviewsAPI/Server/index.js
+++ ReviewsAPI/Server/index.js
@@ -1,18 +1,12 @@
 /* eslint-disable no-console */
 const express = require('express');
 const reviews = require('./routes/reviews');
-const metadata = require('./routes/metadata');
-const helpful = require('./routes/metadata');
-const report = require('./routes/metadata');
 
 const app = express();
 const PORT = process.env.PORT || 1337;
 
 app.use(express.json());
-app.use('/reviews/', reviews);
-app.use('/reviews/meta', metadata);
-app.use('/reviews/:review_id/helpful', helpful);
-app.use('/reviews/:review_id/report', report);
+app.use('/reviews', reviews);
 
 app.get('/', (req, res) => res.send('Hello World'));
 
