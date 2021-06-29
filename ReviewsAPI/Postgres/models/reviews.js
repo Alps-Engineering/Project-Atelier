6f47b8ca36cbb90abb1c4df9f87cd284c92f2fba
timapar <timothy_parrish@hotmail.com>
2021-06-29 18:15:36 -0400

    implement postReviewByProduct and refactor getReviewsByProduct to use promises

--- ReviewsAPI/Postgres/models/reviews.js
+++ ReviewsAPI/Postgres/models/reviews.js
@@ -9,12 +9,13 @@ module.exports.getReviewsByProduct = (product, sort, page, count, cb) => {
   WHERE r.product_id = ${product}
   AND r.reported = false;`;
 
-  pool.query(selectQryStr, (err, result) => {
-    if (err) {
+  pool
+    .query(selectQryStr)
+    .catch((err) => {
       console.error('There was a problem getting the reviews from the db: ', err.stack);
       cb(err);
-    }
-    if (result) {
+    })
+    .then((result) => {
       const results = [];
       result.rows.forEach((review) => {
         const formatedReview = { ...review };
@@ -32,20 +33,54 @@ module.exports.getReviewsByProduct = (product, sort, page, count, cb) => {
         count,
         results,
       });
-    }
-  });
+    });
 };
 
-module.exports.postReviewByProduct = (product, data, cb) => {
-  const insertQryStr = ``;
+module.exports.postReviewByProduct = (data, cb) => {
+  const { product_id, rating, summary, body, recommend, name, email, characteristics, photos } = data;
+  const insertReviewQryStr = `
+  INSERT INTO reviews (product_id, rating, recommend, reviewer_name, reviewer_email, date, summary, body)
+  VALUES (${product_id}, ${rating}, ${recommend}, '${name}', '${email}', '${new Date().toISOString()}', '${summary}', '${body}')
+  RETURNING review_id;`;
 
-  pool.query(insertQryStr, (err, result) => {
-    if (err) {
-      console.error('There was a problem writing the data to the db: ', err.stack);
+  pool
+    .query(insertReviewQryStr)
+    .catch((err) => {
+      console.error('There was a problem writing review to the db: ', err.stack);
       cb(err);
-    }
-    if (result) {
-      cb(null, result);
-    }
-  });
+    })
+    .then((result) => {
+      const chars = JSON.parse(characteristics);
+      const { review_id } = result.rows[0];
+      const charsKeys = Object.keys(chars);
+      const resultPromises = charsKeys.map((char) => {
+        const insertCharReviewsQryStr = `
+          INSERT INTO char_reviews (characteristic_id, review_id, value)
+          VALUES (${char}, ${review_id}, ${chars[char]})
+          RETURNING review_id;`;
+        return pool.query(insertCharReviewsQryStr);
+      });
+      Promise.all(resultPromises)
+        .catch((err) => {
+          console.error('There was a problem posting characterisitics to the db: ', err.stack);
+          cb(err);
+        })
+        .then((result) => {
+          const photosArray = JSON.parse(photos);
+          const { review_id } = result[0].rows[0];
+          const resultPromises = photosArray.map((photo) => {
+            const insertPhotosQryStr = `
+                INSERT INTO photos (review_id, url)
+                VALUES (${review_id}, '${photo}')
+                RETURNING review_id;`;
+            return pool.query(insertPhotosQryStr);
+          });
+          Promise.all(resultPromises)
+            .catch((err) => {
+              console.error('There was a problem posting a photo to the db: ', err.stack);
+              cb(err);
+            })
+            .then((result) => cb(null, result[0].rows[0]));
+        });
+    });
 };
