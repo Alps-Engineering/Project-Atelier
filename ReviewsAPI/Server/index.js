d0c10a111720e3b1318e25c213bda71bc9e67db0
Ubuntu <ubuntu@ip-172-31-5-161.us-east-2.compute.internal>
2021-07-01 18:00:00 +0000

    add endpoint for loader.io verification

--- ReviewsAPI/Server/index.js
+++ ReviewsAPI/Server/index.js
@@ -9,6 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/reviews', reviews);
 
 app.get('/', (req, res) => res.send('Hello World'));
+app.get('/loaderio-5cb2b636946d4a3fe44a56a40fce88a6', (req, res) => res.send('loaderio-5cb2b636946d4a3fe44a56a40fce88a6'));
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
